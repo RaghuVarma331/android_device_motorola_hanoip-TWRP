f17af0977d03721c3be22e6567f086a7a00565c8
RaghuVarma331 <raghuvarma331@gmail.com>
2022-06-02 18:24:53 +0530

    hanoip: build required recovery device modules
    Signed-off-by: RaghuVarma331 <raghuvarma331@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,6 +102,15 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 TARGET_NO_RECOVERY := false
+TARGET_RECOVERY_DEVICE_MODULES += \
+    android.hidl.base@1.0 \
+    ashmemd \
+    ashmemd_aidl_interface-cpp \
+    libashmemd_client \
+    libcap \
+    libion \
+    libpcrecpp \
+    libxml2
 
 # Use mke2fs to create ext4 images
 TARGET_USES_MKE2FS := true
