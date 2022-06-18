6770e297e61d55204bafac08e05b44b523d60d34
RaghuVarma331 <raghuvarma331@gmail.com>
2022-06-18 22:39:46 +0530

    hanoip: Rewrite the entire logic for touchscreen universal fix
    Till date we are depending on outdated stock firmware files and kernel modules from stock. Once after we import them to twrp device source and use the same image or permanent zip on every new stock update or any custom rom will leads to touchscreen bug.
    
    Let me elaborate more , example when we use fastboot boot twrp-xyz.img ( this works perfectly with no touch screen issue because it has the same signature for kernel in splitimg & kernel modules in ramdisk/vendor/lib/modules ) or if it is inline kernel with all modules packed inside then also no problem because kernel will play the role. So till here the temporary twrp boot is clear.
    
    Now if we flash permanent twrp zip on any new stock rom or old stock rom or any custom rom then ofcourse touch won't work as expected because? Well , let's assume iam on stock April 2022 update. Now my twrp permanent zip has December 2021 kernel modules. So i flashed permanent zip on April 2022 stock rom. Now the permanent zip will override the ramdisk with twrp and that contains December 2021 kernel modules. But? Yes , now my splitimg has April kernel so here both signatures are not same so the touch doesn't work on April update due to different modules with different signature.
    
    So what is this new logic? How it works?
    
    Well , instead of importing from stock and using it as permanent is a bad idea. Ofcourse on super dynamic phones no user can wipe vendor individually from fastboot or twrp. Now from this point i just run bootscript from on boot service and mount vendor. Finally copy firmware and modules to /sbin and run the touchscreen logic to make it work. Especially this logic aim is all about to utilise kernel  modules  & firmware files from the ongoing rom on user phone so that we may never encounter touch issue on temporary & twrp permanent zip as well and this will survive for any upcoming Android versions upgrade.
    
    Signed-off-by: RaghuVarma331 <raghuvarma331@gmail.com>

---
+++ recovery/root/system/bin/hanoip_touch.sh
@@ -0,0 +1,28 @@
+#!/system/bin/sh
+
+module_path=/vendor/lib/modules
+firmware_path=/vendor/firmware
+touch_class_path=/sys/class/touchscreen
+insmod $module_path/exfat.ko
+insmod $module_path/ilitek_v3_mmi.ko
+insmod $module_path/mmi_info.ko
+insmod $module_path/moto_f_usbnet.ko
+insmod $module_path/qpnp_adaptive_charge.ko
+insmod $module_path/sx933x_sar.ko
+insmod $module_path/fpc1020_mmi.ko
+insmod $module_path/mmi_annotate.ko
+insmod $module_path/mmi_sys_temp.ko
+insmod $module_path/mpq-adapter.ko
+insmod $module_path/sensors_class.ko
+insmod $module_path/utags.ko
+cd $firmware_path
+touch_product_string=$(ls $touch_class_path)
+echo "ilitek"
+firmware_file="FW_ILITEK_TDDI_TM.bin"
+touch_path=/sys$(cat $touch_class_path/$touch_product_string/path | awk '{print $1}')
+wait_for_poweron
+echo $firmware_file > $touch_path/doreflash
+echo 1 > $touch_path/forcereflash
+sleep 5
+echo 1 > $touch_path/reset
+
--- recovery/root/system/bin/runatboot.sh
+++ recovery/root/system/bin/runatboot.sh
@@ -1,35 +1,24 @@
 #!/system/bin/sh
 
-module_path=/vendor/lib/modules
+# mount points
+path=/sbin
+mount /vendor
+cp -r /vendor/lib/modules $path
+cp -r /vendor/firmware $path
+umount /vendor
 
-touch_class_path=/sys/class/touchscreen
-touch_path=
-firmware_path=/vendor/firmware
-firmware_file=
+# Apply pathces to temporary vendor
+cd /vendor
+mkdir lib
+cp -r $path/modules /vendor/lib
+cp -r $path/firmware /vendor
 
-# Load TouchScreen Modules
-insmod $module_path/exfat.ko
-insmod $module_path/ilitek_v3_mmi.ko
-insmod $module_path/mmi_info.ko
-insmod $module_path/moto_f_usbnet.ko
-insmod $module_path/qpnp_adaptive_charge.ko
-insmod $module_path/sx933x_sar.ko
-insmod $module_path/fpc1020_mmi.ko
-insmod $module_path/mmi_annotate.ko
-insmod $module_path/mmi_sys_temp.ko
-insmod $module_path/mpq-adapter.ko
-insmod $module_path/sensors_class.ko
-insmod $module_path/utags.ko
+# Clean up temporary data
+rm -r $path/modules
+rm -r $path/firmware
 
-cd $firmware_path
-touch_product_string=$(ls $touch_class_path)
-echo "ilitek"
-firmware_file="FW_ILITEK_TDDI_TM.bin"
-touch_path=/sys$(cat $touch_class_path/$touch_product_string/path | awk '{print $1}')
-wait_for_poweron
-echo $firmware_file > $touch_path/doreflash
-echo 1 > $touch_path/forcereflash
-sleep 5
-echo 1 > $touch_path/reset
+# Run TouchScreen Patch
+/system/bin/sh /system/bin/hanoip_touch.sh 2> $path/TouchScreenlog.txt
+/system/bin/sh /system/bin/hanoip_touch.sh 2>> $path/TouchScreenlog.txt
 
 exit 0
